b14d5df5be5dc7214b86c0570cbccf95e360ea82
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2016-10-28 21:18:10 -0200

    Correction, Scene.lua deleted, and some new features

--- src/scenes/SecondScene.lua
+++ src/scenes/SecondScene.lua
@@ -1,11 +1,20 @@
-require '../Scene'
-
-secondScene = Scene.new()
-secondScene.name = 'SecondScene'
-
-function secondScene:Draw()
-  love.graphics.setBackgroundColor(255,0,0)
-  love.graphics.print("Scene:SecondScene",50,50)
+-- obrigatory:
+local Scene = {}
+Scene.__index = Scene
+function Scene:New ()
+    local nw = {}
+    setmetatable(nw, Scene)
+    return nw
 end
 
-secondScene:AddScene(secondScene)
+-- Your Scene functions, have fun :)
+function Scene:Load ()
+end
+
+function Scene:Draw()
+    love.graphics.setBackgroundColor(150,20,20)
+    love.graphics.print("Scene: SecondScene",50,50)
+end
+
+-- This return is also necessary
+return Scene:New()
